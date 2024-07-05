0cc49e3701dd9bf0741f9e6cb06c3570120383e1
0xSoul <31786518+0xSoul@users.noreply.github.com>
2024-07-04 22:40:40 -0500

    Taller 2 2/2

--- Taller 2/admin/routes/fotos.js
+++ Taller 2/admin/routes/fotos.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const Sequelize = require('sequelize');
+const Foto = require('../models').foto;
+
 router.get('/findAll/json',
     function (req, res, next) {
         Foto.findAll({
-            attributes: {
-                exclude: ["updatedAt"]
-            }
-        }).then(fotos => {
-            res.json(fotos);
-        }).catch(error => res.status(400).send(error))
+            attributes: { exclude: ["updatedAt"] }
+        })
+            .then(fotos => {
+                res.json(fotos);
+            })
+            .catch(error => res.status(400).send(error))
     });
 
 router.get('/findAll/view', function (req, res, next) {
--- Taller 2/admin/seeders/20240704130248-fotos.js
+++ Taller 2/admin/seeders/20240704130248-fotos.js
@@ -7,32 +7,15 @@ module.exports = {
       await queryInterface.bulkInsert('fotos', [{
         titulo: 'fotos' + i,
         descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        calificacion:
-          (Math.random() * 10).toFixed(2),
-
+        calificacion: (Math.random() * 10).toFixed(2),
         ruta: 'public/images/' + 'fotos' + i + '.png',
         createdAt: new Date(),
         updatedAt: new Date()
       }], {});
     }
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('fotos', null, {});
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  },
+  }
 };
